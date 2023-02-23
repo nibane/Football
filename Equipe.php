5ca7a270021dc777026ea44115d3a3d577cebc2f
nibane <azari46165@gmail.com>
2023-02-23 10:47:28 +0100

    Je recommence sur de nouvelle base

--- Equipe.php
+++ Equipe.php
@@ -4,23 +4,13 @@
 <?php
 
 class Equipe{
-    
-    private string $nom;
-    private int $creation;
-    private Pays $pays ;
-    private array $joueurs ;
-    private array $saison ;
 
-    function __construct(string $nom, int $creation, Pays $pays)
-    {
-        $this->nom = $nom;
-        $this->creation = $creation;
-        $this->pays = $pays;
-        $this->joueurs = [];
-        $this->saison = [];
-        $pays->ajouterEquipes($this);
-        
-    }
+    //Attribut
+    private string $nomEquipe;
+    private Pays $pays;
+    private DateTime $dateCreation;
+    //historique des joueurs du club
+    private array $saison;
 
 
 
@@ -29,108 +19,15 @@ class Equipe{
 
 
 
-    /**
-     * Get the value of nom
-     */ 
-    public function getNom()
-    {
-        return $this->nom;
-    }
-
-    /**
-     * Set the value of nom
-     *
-     * @return  self
-     */ 
-    public function setNom($nom)
-    {
-        $this->nom = $nom;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of creation
-     */ 
-    public function getCreation()
-    {
-        return $this->creation;
-    }
-
-    /**
-     * Set the value of creation
-     *
-     * @return  self
-     */ 
-    public function setCreation($creation)
-    {
-        $this->creation = $creation;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of pays
-     */ 
-    public function getPays()
-    {
-        return $this->pays;
-    }
-
-    /**
-     * Set the value of pays
-     *
-     * @return  self
-     */ 
-    public function setPays($pays)
-    {
-        $this->pays = $pays;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of joueurs
-     */ 
-    public function getJoueurs()
-    {
-        return $this->joueurs;
-    }
-
-    /**
-     * Set the value of joueurs
-     *
-     * @return  self
-     */ 
-    public function setJoueurs($joueurs)
-    {
-        $this->joueurs = $joueurs;
-
-        return $this;
-    }
-
-    //ajoute objet $joueur dans le tableaux joueurs
-    function ajouterJoueurs($joueur){
-        $this->joueurs[] = $joueur;
-    }
-
-    //affiche une equipe et ses joueurs
-    function afficherEquipes(){
-        $result = "";
-       
-        foreach ($this->saison as $joueur){
-            $result .= $this->getNom()." ".$joueur->getPrenom()." ".$joueur->getDateDebut();
-        }
-        echo $this->nom." <br>".$this->creation."<br>".$result."<br>";
-       
-    }
-
-    function ajouterSaison(Saison $saison){
-        $this->saison[] = $saison;
-    }
 
 
 
+
+
+
+
+
+ 
 }
 
 
--- Pays.php
+++ Pays.php
@@ -5,56 +5,10 @@
 
 class Pays{
     
-    private string $nom ;
-    private array $allEquipes ;
-
-    function __construct(string $nom)
-    {
-        $this->nom = $nom;
-        $this->allEquipes = [];
-    }
-
-    
-
-
-   
-    public function getNom()
-    {
-        return $this->nom;
-    }
-
-    public function setNom($nom)
-    {
-        $this->nom = $nom;
-        return $this;
-    }
-
-    public function getAllEquipes()
-    {
-        return $this->allEquipes;
-    }
-
-    public function setAllEquipes($allEquipes)
-    {
-        $this->allEquipes = $allEquipes;
-        return $this;
-    }
-
-    //sert a integrer une Equipes dans un pays 
-    function ajouterEquipes(Equipe $equipes){
-        $this->allEquipes[] = $equipes;
-    }
-
-    //recupere le nom est la date de toutes les equipes
-    function afficherPays(){
-        $result = "";
-       
-        foreach ($this->allEquipes as $equipes ){
-            $result .= $equipes->getNom()."<br>";
-        }
-       echo "<div class='boite'><div class='titre'>".$this->nom."</div> <div class='bas'>".$result."</div></div><br>";
-    }
-
+   //Attributs
+   private string $nomPays;
+   private array $equipes;
+   private array $joueurs;
 
 
 
